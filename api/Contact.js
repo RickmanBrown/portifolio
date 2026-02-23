86a4d536d870f128cf312cea7f9c58968f416916
Paloma Brito <palomabarbosabrito.dev@gmail.com>
2026-02-23 09:14:20 -0300

    ajustando para o api funcionar

--- api/Contact.js
+++ api/Contact.js
@@ -8,7 +8,9 @@ export default async function handler(req, res) {
   const { name, email, message } = req.body;
 
   const transporter = nodemailer.createTransport({
-    service: "gmail",
+    host: "smtp.gmail.com",
+    port: 465,
+    secure: true, // use TLS
     auth: {
       user: process.env.EMAIL_USER,
       pass: process.env.EMAIL_PASS,
@@ -21,9 +23,9 @@ export default async function handler(req, res) {
       to: process.env.EMAIL_USER,
       subject: "Mensagem do Portfólio",
       text: `
-Nome: ${name}
-Email: ${email}
-Mensagem: ${message}
+      Nome: ${name}
+      Email: ${email}
+      Mensagem: ${message}
       `,
     });
 
